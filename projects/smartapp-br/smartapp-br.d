48c6d81ef762ff99ddb4c2691f11c87625e2ada7
Anuj Sehgal <anuj@iurs.org>
2013-03-08 15:15:58 +0100

    Removed unnecessary files.

--- projects/smartapp-br/smartapp-br.d
+++
@@ -1,5 +0,0 @@
-smartapp-br.elf: obj_redbee-econotag/board.h \
-  ../../cpu/mc1322x/board/redbee-econotag.h \
-  ../../cpu/mc1322x/board/std_conf.h \
-  ../../cpu/mc1322x/src/default_lowlevel.h \
-  ../../cpu/mc1322x/lib/include/crm.h
